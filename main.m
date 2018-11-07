1e27ac83686441f0a53446b45f5ec551fc180a28
Sathyan Padmanabhan <sathyanpadmanabhan@hotmail.com>
2018-11-07 12:12:00 -0700

    Rho2_rho1 ratio is fixed now, have to implement T coefficients

--- main.m
+++ main.m
@@ -9,17 +9,22 @@ u1 = M1*sqrt(1.4*287*T1);
 rho1 = (P1/(287*T1));%kg/m^3
 rho_ratio_old = 0.1;
 rho_ratio_diff = rho_ratio_old;
-while rho_ratio_diff > 1E-3
-  rho2 = rho1*rho_ratio_old;
-  P2 = P1+rho1*u1*u1*(1-rho_ratio_old);
-  h2 = h1+(u1*u1*(1/2)*(1-(rho_ratio_old)^2));
-  X = log(P2/1.013E5);
-  Y = log(rho2/1.225);
-  Z = X-Y;
-  gamma = coefficients(X,Y,Z);
-  rho_ratio_new = ((P2/h2)*(gamma/(gamma-1)))/rho1;
-  rh0_ratio_diff = abs(rho_ratio_old - rho_ratio_new);  
-  rho_ratio_old = rho_ratio_new; 
-end; 
-
-rho2 = rho_ratio_old*rho1;
+iter = 1;
+tol = 1E-10; 
+fileID = fopen('res2.txt','w');
+while rho_ratio_diff > tol
+ rho2 = rho1/rho_ratio_old;
+ P2 = P1+rho1*u1*u1*(1-rho_ratio_old);
+ h2 = h1+(u1*u1*(1/2)*(1-(rho_ratio_old)^2));
+ X = log(P2/1.013E5);
+ Y = log(rho2/1.225);
+ Z = X-Y;
+ gamma = coefficients(X,Y,Z);
+ rho_ratio_new = rho1/((P2/h2)*(gamma/(gamma-1)));
+ rho_ratio_diff = abs(rho_ratio_old - rho_ratio_new);  
+ rho_ratio_old = rho_ratio_new;
+ fprintf(fileID,'%d , %.4f, %.4f, %.4f\n',iter,gamma,real(rho2), imag(rho2));
+ iter = iter+1;
+end 
+fclose(fileID);
+rho2_rho1 = rho2/rho1;
