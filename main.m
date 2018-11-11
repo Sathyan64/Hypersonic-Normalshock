6458c125ed51d7061966f827f4cc9211cb901d7f
Sathyan Padmanabhan <sathyanpadmanabhan@hotmail.com>
2018-11-11 15:11:56 -0700

    Bugs to be fixed
    Temperature and density ratio going wayward. Have to check the estimation of rho2 is right.

--- main.m
+++ main.m
@@ -3,14 +3,14 @@ prompt = 'Incoming Mach number : ';
 M1 = input(prompt);
 prompt2 = 'Altitude in feet : ';
 H_f = input(prompt2);
-H = H_f*0.3048;
-[P1,T1,rho1] = Alt_profile(H);
-gamma1 = 1.4;
-R = 287;
-Cp1 = (gamma1*R)/(gamma1-1);
-h1 = Cp1*T1;
-u1 = M1*sqrt(1.4*287*T1);
+H = H_f*0.3048; % w
+[P1,T1,rho1] = Alt_profile(H);% w
+gamma1 = 1.4;% w
+R = 287;% w
+Cp1 = (gamma1*R)/(gamma1-1);% w
+h1 = Cp1*T1;% w
 
+u1= M1*sqrt(1.4*287*T1);% w
 rho_ratio_old = 0.1;
 rho_ratio_diff = rho_ratio_old;
 iter = 1;
@@ -31,8 +31,18 @@ while rho_ratio_diff > tol
  iter = iter+1;
 end 
 fclose(fileID);
-rho2_rho1 = rho2/rho1;
+rho2_rho1 = rho2/rho1
 gamma_2 = coefficients_T(X,Y,Z);
 T_0_ref = 288.16;
-T2 = T_0_ref*exp(gamma_2);
+T2 = T_0_ref*exp(gamma_2)
+% U1= M1.*sqrt(1.4*287*T1)*3.2804;
+% figure(1)
+% hold on;
+% plot(U1,T2);
+% 
+% figure(2)
+% hold on;
+% plot(U1,rho2_rho1);
+
+
 
